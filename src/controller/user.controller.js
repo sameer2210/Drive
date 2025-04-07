c0e28434f8c00e9df1b6b27dc9c2384ddb831658
sameer2210 <sameerkhan27560@gmail.com>
2025-04-08 00:30:24 +0530

    error_handle

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -5,17 +5,19 @@ const userModel = require("../models/users.models");
 const { validationResult } = require("express-validator");
 
 module.exports.createUserGet = (req, res) => {
-  res.render("register");
+  const {error} = req.query;
+  res.render("register",{error});
 };
 
 module.exports.createUserPost = async (req, res) => {
   const errors = validationResult(req);
   // console.log(errors);
   if (!errors.isEmpty()) {
-    return res.status(400).json({
-      errors: errors.array(),
-      message: "Invalid data Try again"
-    });
+    return res.redirect("/users/?error=Invalid input, please try again.");
+    // return res.status(400).json({
+    //   errors: errors.array(),
+    //   message: "Invalid data Try again"
+    // });
     // res.send(errors);
   }
   try {
@@ -25,7 +27,9 @@ module.exports.createUserPost = async (req, res) => {
 
     if (existUser) {
       // res.send(`user already Exist`);
-      return res.status(400).json({ message: "User already exists" });
+      // return res.status(400).json({ message: "User already exists" });
+      return res.redirect("/users/?error=User already exists, please try again.");
+
     }
     const hashPassword = await bcryptjs.hash(password, 10);
 
@@ -50,26 +54,27 @@ module.exports.createUserPost = async (req, res) => {
   }
 };
 
-// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------
 
 module.exports.loginUserGet = (req, res) => {
-  res.render("login");
+  const { error } = req.query;
+  res.render("login", { error });
 };
 module.exports.loginUserPost = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const existUser = await userModel.findOne({ email });
-
+    // console.log(existUser);
     if (!existUser) {
-      // alert(`user not exist  please register....`);
-      // return res.redirect("/users/?error=User does not exist, please register.");
-      return res.redirect("/users/");
+      // return res.redirect("/users/");
+      return res.redirect("/users/?error=User does not exist, please register or Retry.");
     }
 
     const isMatch = await bcryptjs.compare(password, existUser.password);
     if (!isMatch) {
-      return res.send(`Password not match`);
+      // return res.send(`Password not match`);
+      return res.redirect("/users/login?error=Password does not match, please try again.");
     }
     const token = jwt.sign(
       {
@@ -85,7 +90,7 @@ module.exports.loginUserPost = async (req, res) => {
     //     message: "User registered successfully",
     //   });
   } catch (error) {
-    console.error("Login error:", error); // Optional: log to console
+    console.error("Login error:", error);          // Optional: log to console
     res.status(500).json({ error: error.message });
     // res.redirect('/login')
   }
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../helper/multer");
-const {authMiddleware} = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 const {
   profileGet,
@@ -13,12 +13,11 @@ const {
 router.get("/", (req, res) => {
   res.send(
     `<a href="/users" 
-     style="text-decoration: none; font-weight: bold; color: white; background-color: #007BFF; padding: 12px 20px; border-radius: 8px; display: inline-block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 18px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); transition: all 0.3s ease-in-out;"
+     style=" font-weight: bold; color: white; background-color:rgb(255, 22, 22); padding: 12px 20px; border-radius: 8px; display: inline-block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 18px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); transition: all 0.3s ease-in-out;"
      onmouseover="this.style.backgroundColor='#0056b3'; this.style.transform='translate(-50%, -50%) scale(1.1)';" 
      onmouseout="this.style.backgroundColor='#007BFF'; this.style.transform='translate(-50%, -50%) scale(1)';">
      Get Started
     </a>
-
     `
   );
 });
@@ -29,6 +28,5 @@ router.get("/show", authMiddleware, showGet);
 
 // Handle file upload
 router.post("/upload", authMiddleware, upload, uploadPost);
-
 
 module.exports = router;
