39a1d48410c8ed09b431a2cf9743a791297017b9
sameer2210 <sameerkhan27560@gmail.com>
2025-04-09 01:08:12 +0530

    cloudinary_file

--- src/helper/upload.js
+++ src/helper/upload.js
@@ -1,36 +1,3 @@
-// const cloudinary = require("cloudinary").v2;
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.API_KEY,
-//   api_secret: process.env.API_SECRET
-// });
-
-// // Upload file buffer to Cloudinary
-// const uploadFile = async (fileBuffer) => {
-//   try {
-//     // Upload file directly from buffer
-//     const result = await cloudinary.uploader
-//       .upload_stream(
-//         { resource_type: "auto" }, // Automatically detects the file type (image, video, etc.)
-//         (error, result) => {
-//           if (error) {
-//             console.log(error.message);
-//             return;
-//           }
-//           return result.secure_url;
-//         }
-//       )
-//       .end(fileBuffer); // End the stream with the file buffer
-//     console.log(result, "OKAY");
-//     return result;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-// module.exports = uploadFile;
-
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -40,10 +7,10 @@ cloudinary.config({
 });
 
 // Upload file buffer to Cloudinary
-const uploadFile = (fileBuffer) => {
+const uploadFile = (fileBuffer, options = {}) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { resource_type: "auto" },
+      { resource_type: "auto", ...options },
       (error, result) => {
         if (error) {
           console.error("Cloudinary Upload Error:", error.message);
@@ -56,5 +23,6 @@ const uploadFile = (fileBuffer) => {
     uploadStream.end(fileBuffer);
   });
 };
+
 
 module.exports = uploadFile;
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -14,8 +14,8 @@ router.get("/", (req, res) => {
   res.send(
     `<a href="/users" 
      style=" font-weight: bold; color: white; background-color:rgb(255, 22, 22); padding: 12px 20px; border-radius: 8px; display: inline-block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 18px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); transition: all 0.3s ease-in-out;"
-     onmouseover="this.style.backgroundColor='#0056b3'; this.style.transform='translate(-50%, -50%) scale(1.1)';" 
-     onmouseout="this.style.backgroundColor='#007BFF'; this.style.transform='translate(-50%, -50%) scale(1)';">
+     onmouseover="this.style.backgroundColor='#4F46E5'; this.style.transform='translate(-50%, -50%) scale(1.1)';" 
+     onmouseout="this.style.backgroundColor='#4338CA'; this.style.transform='translate(-50%, -50%) scale(1)';">
      Get Started
     </a>
     `
